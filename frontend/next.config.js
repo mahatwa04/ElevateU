4ec72b0a5fd4aa6327f8518932a73c6b1d8dc5ab
abhinavkumar01aug-crypto <abhinavkumar01aug@gmail.com>
2025-11-27 20:54:20 +0530

    ci: simplify github pages workflow

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/ElevateU' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ElevateU' : '',
   reactStrictMode: true,
 }
 
